5155a013370063750f735eb55b095dc1989afac8
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-04-17 14:23:02 +0200

    Create Q4.php

---
+++ Q4.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start()
+
+function SanitizeInput($input) {
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+}
+
+if ($_SERVER["Request_Method"] == "GET") {
+    if (isset($_GET["username"])) {
+        $username = sanitize_input($_GET["username"]);
+    } else {
+        $username = "";
+    }
+
+    if (isset($_GET["age"])) {
+        $age = sanitize_input($_GET["age"]);
+    } else {
+        $age = "";
+    }
+
+    $_SESSION["username"] = $username;
+    $_SESSION["age"] = $age;
+
+    echo("Done successfully")
+    exit()
+}
+
+elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["username"])) {
+        $username = sanitize_input($_POST["username"]);
+    } else {
+        $username = "";
+    }
+
+    if (isset($_POST["age"])) {
+        $age = sanitize_input($_POST["age"]);
+    } else {
+        $age = "";
+    }
+
+    $_SESSION["username"] = $username;
+    $_SESSION["age"] = $age;
+
+    echo("Done successfully")
+    exit()
+}
+?>
