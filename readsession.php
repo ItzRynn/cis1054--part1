733b741db9d333417bdeae0777f2b867bf5a12e1
JulianZ05 <150332534+JulianZ05@users.noreply.github.com>
2024-05-27 18:24:58 +0200

    Update readsession.php

--- readsession.php
+++ readsession.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 
+//Checking if username and age were set
 if (isset($_SESSION["username"]) && isset($_SESSION["age"])) {
     $username = $_SESSION["username"];
     $age = $_SESSION["age"];
-
+//Outputting username and age if they were set
     echo "Username: $username<br>";
     echo "Age: $age";
 } else {
