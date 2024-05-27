7a975ca83b37207ed1c41e79c903804e3e0fa081
JulianZ05 <150332534+JulianZ05@users.noreply.github.com>
2024-05-27 18:22:48 +0200

    Update lastvisit.php

--- lastvisit.php
+++ lastvisit.php
@@ -3,11 +3,11 @@ session_start();
 
 // Check if the first visit timestamp is stored in the session
 if (!isset($_SESSION['first_visit'])) {
-    // If not, set the timestamp of the first visit
+    // If not, set timestamp of the first visit
     $_SESSION['first_visit'] = time();
     echo "Welcome! This is your first visit.";
 } else {
-    // If the first visit timestamp is stored, calculate the elapsed time
+    // else, calculate the elapsed time
     $first_visit_timestamp = $_SESSION['first_visit'];
     $current_time = time();
     $elapsed_time = $current_time - $first_visit_timestamp;
