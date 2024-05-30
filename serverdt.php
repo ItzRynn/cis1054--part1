e140d7b67127ad84aa67d140251bb291a9af2f1f
ItzRynn <152287136+ItzRynn@users.noreply.github.com>
2024-05-30 19:58:50 +0200

    Update serverdt.php

--- serverdt.php
+++ serverdt.php
@@ -1,11 +1,4 @@
 <?php
-// Set the content type header to text/html for HTML output
-header("Content-type: text/html");
-
-// Get the current date and time
 $currentDateTime = date("Y-m-d H:i:s");
-
+echo "Server Date and Time: $currentDateTime";
 ?>
-
-Server Date and Time: <?php echo $currentDateTime; ?>
-
